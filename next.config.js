26b19210e539d1d11d7e7f3aeb2c3d38fc13978b
Saeideh Tajmehr <saeideh.tajmehr@efsibiz.com>
2025-04-17 12:19:36 +0330

    api

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-};
+  experimental: {
+    serverActions: true // اگه استفاده می‌کنی
+  }
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
